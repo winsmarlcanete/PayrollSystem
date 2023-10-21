4e6f9d4ae7ce5ad7002be255916f78808638e025
Windows <Windows@The-King>
2023-10-21 23:12:11 +0800

    Apache Netbeans commit

---
+++ src/main/java/com/mycompany/wp2c2project/WP2C2Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.wp2c2project;
+
+/**
+ *
+ * @author Windows
+ */
+public class WP2C2Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
