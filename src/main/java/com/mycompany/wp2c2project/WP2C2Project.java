f9a33fe5df3c409754dde0826c14fe9ee1731e69
Winsmarl Cañete <Windows@The-King>
2023-10-22 19:11:48 +0800

    employee account na ginawa ni winsmarl

--- src/main/java/com/mycompany/wp2c2project/WP2C2Project.java
+++ src/main/java/com/mycompany/wp2c2project/WP2C2Project.java
@@ -11,14 +11,9 @@ package com.mycompany.wp2c2project;
 public class WP2C2Project {
 
     public static void main(String[] args) {
-        RegisterFrame rf = new RegisterFrame();
-        rf.setVisible(true);
-        // hilo
-        //oks na ba?
-        //sakit sa ulo neto
-        // 3:17pm tangina hirap naman nito
-        //auq na T_T
-        //so need mag pull lagi bago mag commit????
+        EmployeeAccount ea = new EmployeeAccount();
+        ea.setVisible(true);
+        
         
     }
 }
