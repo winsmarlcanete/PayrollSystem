d26a0e48857808cdb0288865912630355a54e670
jejer <jejer@Huawei-MateBook-D-15>
2023-11-23 18:36:43 +0800

    Reading employee info from excel and registering done. Automated database setup done. (uhhh..)

---
+++ src/main/java/com/mycompany/wp2c2_payroll_new/WP2C2_Payroll_New.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.wp2c2_payroll_new;
+
+/**
+ *
+ * @author jejer
+ */
+public class WP2C2_Payroll_New {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
